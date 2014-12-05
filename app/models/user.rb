4665de733ec95b544315fefad3f4829033f1aed4
Yen Tran <yentran@Yens-MacBook-Pro-4.local>
2014-12-05 15:24:10 -0800

    Remove crud from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,31 +1,3 @@
 class User < ActiveRecord::Base
 
-	def index
-
-	end
-
-	def show
-
-	end
-
-	def new
-
-	end
-
-	def edit
-
-	end
-
-	def create
-
-	end
-
-	def update
-
-	end
-
-	def destroy
-
-	end
-
 end
