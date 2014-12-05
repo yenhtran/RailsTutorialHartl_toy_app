a57750a6380687c2d5e8003024c9bf0657ef7c7f
Yen Tran <yentran@Yens-MacBook-Pro-4.local>
2014-12-05 15:13:52 -0800

    Create User model and routes

---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ActiveRecord::Base
+
+	def index
+
+	end
+
+	def show
+
+	end
+
+	def new
+
+	end
+
+	def edit
+
+	end
+
+	def create
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+
+end
