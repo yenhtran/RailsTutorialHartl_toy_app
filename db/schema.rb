5f0188dad91261a2f469c64c787971dd9a4c0479
Yen Tran <yentran@Yens-MacBook-Pro-4.local>
2014-12-05 15:25:08 -0800

    Create micropost model and resouce

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141205230411) do
+ActiveRecord::Schema.define(version: 20141205231917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "microposts", force: true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
